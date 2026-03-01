3e33b9ba20109683ee65a2b9c45259d0ec6613be
Hans Flores <floressktt11@gmail.com>
2026-03-02 00:42:22 +0800

    test

--- welcom.php
+++ welcom.php
@@ -18,7 +18,7 @@
 <body class="bg-gray-100">
   <header class="bg-blue-500 text-white p-4">
   <div class="container mx-auto">
-    <h1 class="text-3xl font-semibold">Condition Statement</h1>
+    <h1 class="text-3xl font-semibold">Condition Statement hanskit dayo</h1>
   </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
